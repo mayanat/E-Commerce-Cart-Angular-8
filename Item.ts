727ed10f24b6c2d029113f45cb272e81726c1c5f
Mayan Atir <mayanfigura@gmail.com>
2019-11-18 09:46:58 +0200

    Add files via upload

---
+++ Item.ts
@@ -0,0 +1,11 @@
+import { ToyC } from './ToyC';
+
+export class Item{
+    toy:ToyC;
+    quantity :number = 0;
+    constructor(){
+        this.quantity=0;
+    }
+
+
+}
---
+++ ToyC.ts
@@ -0,0 +1,13 @@
+export class ToyC{
+    category: string;
+    categoryNum: string;
+    categoryImg: string;
+    tName:string;
+    tPrice: number;
+    tOnSale:boolean=false;
+    tSalePrice: number=-1;
+    tDescription:string;
+    tImg:string;
+
+
+}
